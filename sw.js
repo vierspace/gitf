c394804ab55b7b61cc2da8665a0395f716092fef
vierspace <65929916+vierspace@users.noreply.github.com>
2025-04-22 11:42:16 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      '/pwa-examples/a2hs/',
-      '/pwa-examples/a2hs/index.html',
-      '/pwa-examples/a2hs/index.js',
-      '/pwa-examples/a2hs/style.css',
-      '/pwa-examples/a2hs/images/fox1.jpg',
-      '/pwa-examples/a2hs/images/fox2.jpg',
-      '/pwa-examples/a2hs/images/fox3.jpg',
-      '/pwa-examples/a2hs/images/fox4.jpg',
+      '/gitf/',
+      '/gitf/index.html',
+      '/gitf/index.js',
+      '/gitf/style.css',
+      '/gitf/images/fox1.jpg',
+      '/gitf/images/fox2.jpg',
+      '/gitf/images/fox3.jpg',
+      '/gitf/images/fox4.jpg',
     ])),
   );
 });
